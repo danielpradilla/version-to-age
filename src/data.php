e47c4e61a44d30344ae0b30ead2b385b89a69185
Peter Kahl <peterkahl@users.noreply.github.com>
2018-04-01 07:27:55 +0100

    Create data.php

---
+++ src/data.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Timestamp when this date were updated.
+ * @var string
+ */
+$timestamp = 1513032357; # Tuesday, 12 December 2017 06:45:57 GMT+08:00
+
+/**
+ * Most recent version of browsers.
+ * https://en.wikipedia.org/wiki/Timeline_of_web_browsers
+ * @var array
+ */
+$browsers = array(
+  'chrome'         => '',
+  'crios'          => '',
+  'edge'           => '40.15063',
+  'explorer'       => '11.0.46',
+  'lunascape'      => '6.15.0',
+  'maxthon'        => '5.1.2',
+  'mobile_safari'  => '11.0',
+  'safari'         => '11.0.1',
+  'netsurf'        => '3.7',
+  'opera'          => '49.0',
+  'samsungbrowser' => '6.2',
+  'seamonkey'      => '2.49',
+);
+
+/**
+ * Most recent version of OS.
+ * @var array
+ */
+$osystems = array(
+  'android' => '8.0',
+  'ios'     => '11.3',
+  'macos'   => '10.13.2',
+  'windows' => '10.0',
+);
+
+
