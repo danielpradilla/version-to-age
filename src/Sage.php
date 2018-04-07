fef171638ded5f2f729645c7b046b6bf93b30424
Peter Kahl <peterkahl@users.noreply.github.com>
2018-04-07 07:19:24 +0100

    Update Sage.php

--- src/Sage.php
+++ src/Sage.php
@@ -246,62 +246,30 @@ class Sage {
   #===================================================================
 
   /**
-   * Returns name of the class.
-   * @return string
-   */
-  public function GetClassName() {
-    return __CLASS__;
-  }
-
-  #===================================================================
-
-  /**
-   * Fetches latest data on Firefox and Chrome and adds these
-   * to the browser array.
-   */
-  private function GetBrowserInfoAll() {
-
-    $chr = $this->getLatestVersionChrome();
-    $fox = $this->getLatestVersionFirefox();
-
-    $this->browsers['chrome']  = $chr['version'];
-    $this->browsers['crios']   = $chr['version'];
-    $this->browsers['firefox'] = $fox['version'];
-
-    $this->released = max($fox['released'], $chr['released'], $this->released);
-  }
-
-  #===================================================================
-
-  /**
-   * Returns data on specific browser.
+   * Returns data on specific software.
    * @param  string $br
    * @return array
    */
-  private function GetBrowserInfo($br) {
-
-    $br = $this->strlower($br);
-
+  private function GetInfo($name) {
+    $name = $this->strlower($name);
     if ($this->FetchRemoteData) {
-      if ($br == 'chrome') {
+      if ($name == 'chrome') {
         return self::getLatestVersionChrome();
       }
-      elseif ($br == 'crios') {
+      elseif ($name == 'crios') {
         return self::getLatestVersionChrome();
       }
-      elseif ($br == 'firefox') {
+      elseif ($name == 'firefox') {
         return self::getLatestVersionFirefox();
       }
     }
-
-    if (array_key_exists($br, $this->browsers)) {
+    if (array_key_exists($name, $this->data)) {
       return array(
-        'version'    => $this->browsers[$br],
+        'version'    => $this->data[$name],
         'released'   => $this->released,
         'last_check' => $this->last_check,
       );
     }
-
     return array();
   }
 
@@ -311,83 +279,15 @@ class Sage {
    *
    *
    */
-  public function getBrCurrentVer($br) {
-
-    $br = $this->strlower($br);
-
-    if ($br == '') {
-      return '';
-    }
-
-    $found = $this->GetBrowserInfo($br);
-
-    if (!empty($found['version'])) {
-      return $found['version'];
-    }
-
-    return ''; # uncommon browser
-  }
-
-  #===================================================================
-
-  /**
-   *
-   *
-   */
-  public function getOsCurrentVer($os) {
-
-    $os = $this->strlower($os);
-
-    if ($os == '') {
-      return '';
-    }
-
-    if (!array_key_exists($os, $this->osystems)) {
-      return '';
-    }
-
-    return $this->osystems[$os];
-  }
-
-  #===================================================================
-
-  /**
-   *
-   *
-   */
-  public function isBrOutdated($name, $ver = '') {
-
+  public function getCurrentVer($name) {
     $name = $this->strlower($name);
-
-    if ($name == '' || $ver == '') {
-      return array(
-        'bool'   => false,
-        'factor' => 0,
-      );
+    if ($name == '') {
+      return '';
     }
-
-    $master = $thus->getBrCurrentVer($name);
-
-    if (empty($master)) {
-      return array(
-        'bool'   => false, # uncommon browser
-        'factor' => 0,
-      );
+    if (!array_key_exists($name, $this->data)) {
+      return '';
     }
-
-    $outdated = $this->VersionDistance($ver, $master);
-
-    if ($outdated > 1) {
-      return array(
-        'bool'   => true,
-        'factor' => $outdated,
-      );
-    }
-
-    return array(
-      'bool'   => false, # current
-      'factor' => $outdated,
-    );
+    return $this->data[$os]; # need to get the largest
   }
 
   #===================================================================
@@ -396,151 +296,8 @@ class Sage {
    *
    *
    */
-  public function isOsOutdated($name, $ver = '') {
-
+  public function isOutdated($ver, $name) {
     $name = $this->strlower($name);
-
-    if ($name == '' || $ver == '') {
-      return array(
-        'bool'   => false,
-        'factor' => 0,
-      );
-    }
-
-    if (!array_key_exists($name, $this->osystems)) {
-      return array(
-        'bool'   => false, # uncommon OS
-        'factor' => 0,
-      );
-    }
-
-    if ($name == 'windows') {
-      #  NT        Windows
-      #------------------
-      # 10.0 ..... 10
-      #  6.3 ..... 8.1
-      #  6.2 ..... 8.0
-      #  6.1 ..... 7.0
-      #  6.0 ..... Vista
-      #  5.2 ..... XP
-      #  5.1 ..... XP
-      #  5.0 ..... 2000
-      if (version_compare($ver, '10.0', '>=')) {
-        return array(
-          'bool'     => false, # current
-          'factor'   => 0,
-          'released' => 1438128000, # 29 July 2015
-        );
-      }
-      if (version_compare($ver, '6.3', '>=')) { # Windows 8.1
-        return array(
-          'bool'     => false,
-          'factor'   => .5,
-          'released' => 1396915200, # 8 April 2014
-        );
-      }
-      if (version_compare($ver, '6.2', '>=')) { # Windows 8.0
-        return array(
-          'bool'     => false,
-          'factor'   => .81,
-          'released' => 1351209600, # 26 October 2012
-        );
-      }
-      if (version_compare($ver, '6.1', '>=')) { # Windows 7.0
-        return array(
-          'bool'     => false,
-          'factor'   => .88,
-          'released' => 1256169600, # 22 Oct 2009
-        );
-      }
-      if (version_compare($ver, '6.0', '>=')) { # Vista
-        return array(
-          'bool'     => true,
-          'factor'   => 4,
-          'released' => 1164844800, # 30 Nov 2006
-        );
-      }
-      if (version_compare($ver, '5.2', '>=')) { # XP
-        return array(
-          'bool'     => true,
-          'factor'   => 5,
-          'released' => 1048809600, # 28 March 2003
-        );
-      }
-      # Older, use default
-      return array(
-        'bool'     => true,
-        'factor'   => 6,
-        'released' => 946684800, # 1 Jan 2000
-      );
-    }
-
-    $distance = $this->VersionDistance($ver, self::$osystems[$name]);
-
-    if ($distance > 1) {
-      $outdated = true;
-    }
-    else {
-      $outdated = false;
-    }
-
-    return array(
-      'bool'   => $outdated,
-      'factor' => $distance,
-    );
-  }
-
-  #===================================================================
-
-  /**
-   *
-   *
-   */
-  public static function VersionDistance($test, $master) {
-
-    if ($test === $master) {
-      return 0;
-    }
-    
-    if (substr_count('.', $master) < 1) {
-      $master = $master .'.0';
-    }
-    
-    if (substr_count('.', $test) < 1) {
-      $test = $test .'.0';
-    }
-
-    $master = explode('.', $master);
-    $test   = explode('.', $test);
-
-    #    major  minor  build  patch
-    #         \  \    /     /
-    #          \  |  |     /
-    #           | |  |    |
-    # Version: 62.0.3202.89
-    # $n ....   0 1 2
-    # We're concerned only with major & minor!
-
-    for ($n = 0; $n < 2; $n++) {
-      $test[$n]   = (integer) $test[$n];
-      $master[$n] = (integer) $master[$n];
-    }
-
-    # Normalise values
-    $big = max($test[1], $master[1]);
-    $test[1]   = $test[1]  /($big*1.01);
-    $master[1] = $master[1]/($big*1.01);
-    
-    $t = $test[0]   + $test[1];
-    $m = $master[0] + $master[1];
-
-    # Subtract
-    $diff = $m - $t;
-
-    if ($diff > 0) {
-      return $diff;
-    }
-    return 0;
   }
 
   #===================================================================
@@ -550,13 +307,6 @@ class Sage {
    *
    */
   public function getLatestVersionChrome() {
-
-    $filename = $this->CacheDir .'/'. self::FILEPREFIX .'VER_CHROME_STABLE.json';
-
-    if (!this->$force && file_exists($filename) && filemtime($filename) > time() - 2*3600) {
-      return json_decode(FileGetContents($filename), true);
-    }
-
     $this->curlm->ForcedCacheMaxAge = -1;
     $answer = $this->curlm->Request(self::URLB); # CSV file
     $body   = $answer['body'];
@@ -570,34 +320,20 @@ class Sage {
       # os,channel,current_version,previous_version,current_reldate,previous_reldate,branch_base_commit,branch_base_position,branch_commit,true_branch,v8_version
       # mac,stable,62.0.3202.89,62.0.3202.75,11/06/17,10/26/17,fa6a5d87adff761bc16afc5498c3f5944c1daa68,499098,ba7a0041073a5e9928d277806bfe24c325d113e5,3202,6.2.414.40
       # 0     1         2            3          4         5
-
       foreach ($body as $line) {
         if (substr($line, 0, 10) == 'mac,stable') {
           $temp = explode(',', $line);
         }
       }
-
       if (!isset($temp)) {
         throw new Exception('Unable to find line starting with "mac,stable"');
       }
-
       list($month, $day, $year) = explode('/', $temp[4]);
-
-      $arr = array(
-        'version'    => $temp[2],
-        'released'   => strtotime('20'. $year .'-'. $month .'-'. $day.' 00:00:00 GMT'),
-        'last_check' => time(),
+      return array(
+        'version'  => $temp[2],
+        'released' => strtotime('20'. $year .'-'. $month .'-'. $day.' 00:00:00 GMT'),
       );
-
-      FilePutContents($filename, json_encode($arr, JSON_UNESCAPED_UNICODE), LOCK_EX);
-      return $arr;
     }
-
-    # Try using the aged cache file
-    if (file_exists($filename)) {
-      return unserialize(FileGetContents($filename));
-    }
-
     return array();
   }
 
@@ -608,26 +344,13 @@ class Sage {
    *
    */
   public function getLatestVersionFirefox() {
-
-    $filename = $this->CacheDir .'/'. self::FILEPREFIX .'VER_FIREFOX_STABLE.json';
-
-    if (!$this->force && file_exists($filename) && filemtime($filename) > time() - 2*3600) {
-      return json_decode(FileGetContents($filename), true);
-    }
-
     $all = $this->getAllVersionsFirefox();
     $version = end($all);
-
     $epoch = $this->getEpochFirefoxVersion($version);
-
-    $arr = array(
-      'version'    => $version,
-      'released'   => $epoch,
-      'last_check' => time(),
+    return array(
+      'version'  => $version,
+      'released' => $epoch,
     );
-
-    FilePutContents($filename, json_encode($arr, JSON_UNESCAPED_UNICODE), LOCK_EX);
-    return $arr;
   }
 
   #===================================================================
@@ -667,13 +390,13 @@ class Sage {
    * @return integer
    */
   public function getEpochFirefoxVersion($ver) {
-    $new = array();
     $this->curlm->ForcedCacheMaxAge = -1;
     $answer = $this->curlm->Request(self::URLC . $ver .'/');
     $body   = $answer['body'];
     $status = $answer['status'];
     unset($answer);
     if ($status == '200') {
+      $new = array();
       $body = self::StripHtmlTags($body);
       $body = str_replace("\r", "\n", $body);
       $body = preg_replace("/\n\n+/", "\n", $body);
@@ -695,36 +418,6 @@ class Sage {
    *
    *
    */
-  public static function getBrowserLogo($browser, $pixelDim) {
-    $browser = self::strlower($browser);
-    $file = __DIR__ .'/svg/browser/'. $browser .'.svg';
-    if (file_exists($file)) {
-      return '<img src="data:image/svg+xml;base64,'. base64_encode(FileGetContents($file)) .'" width="'. $pixelDim .'" height="'. $pixelDim .'">';
-    }
-    return '';
-  }
-
-  #===================================================================
-
-  /**
-   *
-   *
-   */
-  public static function getOSLogo($os, $pixelDim) {
-    $os = self::strlower($os);
-    $file = __DIR__ .'/svg/os/'. $os .'.svg';
-    if (file_exists($file)) {
-      return '<img src="data:image/svg+xml;base64,'. base64_encode(FileGetContents($file)) .'" width="'. $pixelDim .'" height="'. $pixelDim .'">';
-    }
-    return '';
-  }
-
-  #===================================================================
-
-  /**
-   *
-   *
-   */
   private static function strlower($str) {
     return str_replace(' ', '_', strtolower($str));
   }
@@ -736,11 +429,9 @@ class Sage {
    *
    */
   private static function StripHtmlTags($str) {
-
     $str = html_entity_decode($str);
     $str = str_replace('<BODY>', '<body>', $str);
     $str = self::EndExplode('<body>', $str);
-
     # Strip HTML
     $str = preg_replace('#<br[^>]*?>#siu',                  "\n", $str);
     $str = preg_replace('#<style[^>]*?>.*?</style>#siu',      '', $str);
@@ -753,11 +444,9 @@ class Sage {
     $str = preg_replace('#<noembed[^>]*?.*?</noembed>#siu',   '', $str);
     $str = preg_replace('#<figcaption>.+</figcaption>#siu',   '', $str);
     $str = strip_tags($str);
-
     # Trim whitespace
     $str = str_replace("\t", '', $str);
     $str = preg_replace('/\ +/', ' ', $str);
-
     return trim($str);
   }
 
