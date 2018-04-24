2c9d1e918ab51b7f94ef0f47d0f62c1ab9b5f1c1
Peter Kahl <peter.kahl@colossalmind.com>
2018-04-24 12:32:43 +0100

    Initial release

--- src/Version2age.php
+++ src/Version2age.php
@@ -1,8 +1,24 @@
 <?php
 /**
+ * Version To Age
+ * Estimates age of browser and OS software.
+ *
+ * @version    2018-04-16 18:00:01 GMT
  * @author     Peter Kahl <https://github.com/peterkahl>
- * @copyright  2008-2018 Peter Kahl
- * @license    Apache License, Version 2.0, http://www.apache.org/licenses/LICENSE-2.0
+ * @copyright  2018 Peter Kahl
+ * @license    Apache License, Version 2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      <http://www.apache.org/licenses/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 namespace peterkahl\Version2age;
@@ -22,7 +38,7 @@ class Version2age {
    * GitHub URL to fetch latest data
    * @var string
    */
-  const URLA = 'https://github.com/peterkahl/Version2age/src/data.json';
+  const URLA = 'https://raw.githubusercontent.com/peterkahl/version-to-age/master/src/data.json';
 
   /**
    * URL to fetch latest data on Chrome
@@ -57,7 +73,7 @@ class Version2age {
 
   /**
    * Enable fetching data from remote hosts.
-   * If true, data is fetched remotely and 
+   * If true, data is fetched remotely and
    * stored in cache.
    * If false, only local data file is used, caching
    * is not used.
@@ -82,7 +98,7 @@ class Version2age {
    * @var integer
    */
   private $last_check;
-  
+
   #===================================================================
 
   /**
@@ -91,7 +107,7 @@ class Version2age {
    * Data array is updated if outdated.
    * Data are stored in cache.
    * @param  boolean $force ... Disables reading of cache, thus forcing
-   *                            new requests to remote hosts to fetch 
+   *                            new requests to remote hosts to fetch
    *                            fresh data.
    * @throws \Exception
    */
